1cdde88c9f4a95cc341bcfcc987deeecc36d5bb3
Donovan Young <donovan.young@constantcontact.com>
2022-04-27 17:41:08 -0400

    Add greet to hello_controller.jsx

--- app/javascript/controllers/hello_controller.jsx
+++ app/javascript/controllers/hello_controller.jsx
@@ -4,7 +4,9 @@ import { createRoot } from "react-dom/client";
 import Hello from "../components/Hello";
 
 export default class extends Controller {
+  static values = { greet: String };
+
   connect() {
-    createRoot(this.element).render(<Hello />);
+    createRoot(this.element).render(<Hello greet={this.greetValue} />);
   }
 }
